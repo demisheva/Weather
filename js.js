77e3895a77b4c61ea9a921c97489a833f0039ddd
Anna Demisheva <demisheva.anna@gmail.com>
2020-05-26 11:23:26 +0200

    deleted comments

--- js.js
+++ js.js
@@ -1,5 +1,5 @@
 
-// take all cities list from json !!!working with Server, for example Live Server
+// take all cities list from json !!!working with Server
 let cityList;
 var request = new XMLHttpRequest();
 request.open('GET', 'city.list.json');
@@ -10,8 +10,6 @@ request.onload = function () {
     return cityList
 }
 
-
-
 function takeDomElement(domElement) {
     return document.querySelector(domElement);
 }
@@ -20,7 +18,7 @@ function fechRequest(cityId) {
     fetch(`http://api.openweathermap.org/data/2.5/weather?id=${cityId}&appid=811b26bdde41b08213dc84b03e747002`)
         .then(function (resp) { return resp.json() })
         .then(function (data) {
-            console.log(data)
+            // console.log(data)
             takeDomElement('#town-state-country').textContent = `${data.name} ${data.sys.country}`;
             takeDomElement('#temperature-active').innerHTML = Math.round(data.main.temp - 273) + '&deg;';
             takeDomElement('#temperature-feels').innerHTML = Math.round(data.main.feels_like - 273) + '&deg;';
@@ -31,7 +29,7 @@ function fechRequest(cityId) {
             takeDomElement('#pressure').textContent = data.main.pressure;
             takeDomElement('#wind').textContent = data.wind.speed;
             takeDomElement('#weather-discription').textContent = data.weather[0].description.toUpperCase();
-            // document.querySelector('#weather-image').src = `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`;
+
             changingWeatherImage(data);
             changeBackgroungImage(data);
 
@@ -39,14 +37,12 @@ function fechRequest(cityId) {
         .catch(function () {
             // catch any errors
             console.log('error')
-            // takeDomElement('.bubble-alert').classList.remove('hiden');
-            // takeDomElement('.find-city').oninput = setTimeout(function () { takeDomElement('.bubble-alert').classList.add('hiden') }, 5000);
         });
 
 
 }
 
-function choseCity() {
+function chooseCity() {
     let city = takeDomElement('.find-city');
     let cityArray = [];
 
@@ -126,10 +122,10 @@ function changeBackgroungImage(data) {
     }
 }
 
-takeDomElement('.find-city-btn').addEventListener('click', choseCity);
-takeDomElement('.find-city').addEventListener("keypress", event => { if (event.keyCode == 13) { choseCity() } });
+takeDomElement('.find-city-btn').addEventListener('click', chooseCity);
+takeDomElement('.find-city').addEventListener("keypress", event => { if (event.keyCode == 13) { chooseCity() } });
 
 document.onload = fechRequest('703448');
-document.onload = alert('!!!working with Server, for example Live Server')
+
 let options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
 takeDomElement('#date').innerHTML = new Date().toLocaleDateString('en-US', options);
